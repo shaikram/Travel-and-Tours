ead0c35291963751824752908e4f8e8d7482df1b
shaikram <shaikram02@gmail.com>
2023-01-01 09:57:27 -0800

    Adding Contact Form Functionality and Booking

---
+++ includes/packages_section.php
@@ -0,0 +1,34 @@
+<!-- Packages-->
+<section class="page-section" id="packages">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-white text-uppercase pb-5">Upcomming Tour Packages</h2>
+        </div>
+        <div class="row text-center">
+            <div class="col-md-4">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x text-white"></i>
+                    <i class="fas fa-shopping-cart fa-stack-1x fa-inverse text-purple"></i>
+                </span>
+                <h4 class="my-3 text-white">E-Commerce</h4>
+                <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            </div>
+            <div class="col-md-4">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x text-white"></i>
+                    <i class="fas fa-laptop fa-stack-1x fa-inverse text-purple"></i>
+                </span>
+                <h4 class="my-3 text-white">Responsive Design</h4>
+                <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            </div>
+            <div class="col-md-4">
+                <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x text-white"></i>
+                    <i class="fas fa-lock fa-stack-1x fa-inverse text-purple"></i>
+                </span>
+                <h4 class="my-3 text-white">Web Security</h4>
+                <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            </div>
+        </div>
+    </div>
+</section>
