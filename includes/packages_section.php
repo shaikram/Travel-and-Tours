e7998cecbc7782963e4f12ded8c690dfd1fbd275
shaikram <shaikram02@gmail.com>
2023-01-02 20:46:19 -0800

    Modify Package Section and add database

--- includes/packages_section.php
+++ includes/packages_section.php
@@ -8,26 +8,86 @@
             <div class="col-md-4">
                 <span class="fa-stack fa-4x">
                     <i class="fas fa-circle fa-stack-2x text-white"></i>
-                    <i class="fas fa-shopping-cart fa-stack-1x fa-inverse text-purple"></i>
+                    <!-- <i class="fas fa-shopping-cart fa-stack-1x fa-inverse text-purple"></i> -->
+                    <i class="fa-stack-1x fa-inverse text-purple mt-4 mr-1" style="font-size: 2.5rem;">3D2N</i>
                 </span>
-                <h4 class="my-3 text-white">E-Commerce</h4>
-                <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                <h4 class="my-3 text-white">Ultimate Package / Joiner Tour</h4>
+                <ol>
+                    <li class="text-white" style="list-style: none;">P5,499 min of 5pax in 1room</li>
+                    <li class="text-white" style="list-style: none;">P5,599 min of 4pax in 1room</li>
+                    <li class="text-white" style="list-style: none;">P5,699 min of 3pax in 1room</li>
+                    <li class="text-white" style="list-style: none;">P6,099 min of 2pax in 1room</li>
+                </ol>
+                <h5 class="text-white">Inclusions</h5>
+                <ul>
+                    <li class="text-white" style="float: left;">3d2n aircon room with own toilet, bath and toiletries & bath towels MARIAFE INN</li>
+                    <li class="text-white" style="float: left;">Daily breakfast and roundtrip airport transfer</li>
+                    <li class="text-white" style="float: left;">Underground river lunch</li>
+                    <li class="text-white" style="float: left;">Honda bay tour with lunch</li>
+                    <li class="text-white" style="float: left;">City tour with light snacks</li>
+                    <li class="text-white" style="float: left;">Tour guide</li>
+                    <li class="text-white" style="float: left;">Tour pickup and drop</li>
+                    <li class="text-white" style="float: left;">Accidental insurance</li>
+                    <li class="text-white" style="float: left;">All entrances</li>
+                </ul>
             </div>
             <div class="col-md-4">
                 <span class="fa-stack fa-4x">
                     <i class="fas fa-circle fa-stack-2x text-white"></i>
-                    <i class="fas fa-laptop fa-stack-1x fa-inverse text-purple"></i>
+                    <!-- <i class="fas fa-shopping-cart fa-stack-1x fa-inverse text-purple"></i> -->
+                    <i class="fa-stack-1x fa-inverse text-purple mt-4 mr-1" style="font-size: 2.5rem;">4D3N</i>
                 </span>
-                <h4 class="my-3 text-white">Responsive Design</h4>
-                <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                <h4 class="my-3 text-white">Ultimate Package / Joiner Tour</h4>
+                <ol>
+                    <li class="text-white" style="list-style: none;">P5,899 min of 5pax in 1room</li>
+                    <li class="text-white" style="list-style: none;">P6,099 min of 4pax in 1room</li>
+                    <li class="text-white" style="list-style: none;">P6,299 min of 3pax in 1room</li>
+                    <li class="text-white" style="list-style: none;">P6,699 min of 2pax in 1room</li>
+                </ol>
+                <h5 class="text-white">Inclusions</h5>
+                <ul>
+                    <li class="text-white" style="float: left;">4d3n aircon room with own toilet, bath and toiletries & bath towels MARIAFE INN</li>
+                    <li class="text-white" style="float: left;">Daily breakfast and roundtrip airport transfer</li>
+                    <li class="text-white" style="float: left;">Underground river lunch</li>
+                    <li class="text-white" style="float: left;">Honda bay tour with lunch</li>
+                    <li class="text-white" style="float: left;">City tour with light snacks</li>
+                    <li class="text-white" style="float: left;">Tour guide</li>
+                    <li class="text-white" style="float: left;">Tour pickup and drop</li>
+                    <li class="text-white" style="float: left;">Accidental insurance</li>
+                    <li class="text-white" style="float: left;">All entrances</li>
+                </ul>
             </div>
             <div class="col-md-4">
                 <span class="fa-stack fa-4x">
                     <i class="fas fa-circle fa-stack-2x text-white"></i>
-                    <i class="fas fa-lock fa-stack-1x fa-inverse text-purple"></i>
+                    <!-- <i class="fas fa-shopping-cart fa-stack-1x fa-inverse text-purple"></i> -->
+                    <i class="fa-stack-1x fa-inverse text-purple mt-4 mr-1" style="font-size: 2.5rem;">5D4N</i>
                 </span>
-                <h4 class="my-3 text-white">Web Security</h4>
-                <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                <div class="p-1">
+                    <h4 class="my-3 text-white">Ultimate Package / Joiner Tour</h4>
+                    <ol>
+                        <li class="text-white" style="list-style: none;">P6,299 min of 5pax in 1room</li>
+                        <li class="text-white" style="list-style: none;">P6,599 min of 4pax in 1room</li>
+                        <li class="text-white" style="list-style: none;">P6,899 min of 3pax in 1room</li>
+                        <li class="text-white" style="list-style: none;">P7,399 min of 2pax in 1room</li>
+                    </ol>
+                </div>
+               
+                <div class="p-1">
+                   
+                    <ul>
+                        <li style="list-style: none;"><h5 class="text-white">Inclusions</h5></li>
+                        <li class="text-white" style="float: left;">3d2n aircon room with own toilet, bath and toiletries & bath towels MARIAFE INN</li>
+                        <li class="text-white" style="float: left;">Daily breakfast and roundtrip airport transfer</li>
+                        <li class="text-white" style="float: left;">Underground river lunch</li>
+                        <li class="text-white" style="float: left;">Honda bay tour with lunch</li>
+                        <li class="text-white" style="float: left;">City tour with light snacks</li>
+                        <li class="text-white" style="float: left;">Tour guide</li>
+                        <li class="text-white" style="float: left;">Tour pickup and drop</li>
+                        <li class="text-white" style="float: left;">Accidental insurance</li>
+                        <li class="text-white" style="float: left;">All entrances</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
