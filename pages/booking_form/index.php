a2e4b3794d2e382da259a8ae9514e7f38faef7a0
shaikram <shaikram02@gmail.com>
2023-01-04 20:57:38 -0800

    Last Commit

--- pages/booking_form/index.php
+++ pages/booking_form/index.php
@@ -75,48 +75,42 @@ if($_GET['status'] === 'true' && $_GET['isExpired'] === 'true') {
             <form>
                 <div class="form-row">
                     <div class="form-group col-md-6">
-                    <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                        <label for="inputEmail4">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="Email" minlength="5" maxlength="20" required>
                     </div>
                     <div class="form-group col-md-6">
-                    <label for="inputPassword4">Password</label>
-                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                        <label for="inputEmail4">Contact no:</label>
+                        <input type="number" class="form-control" id="contact" placeholder="Contact no:" minlength="5" maxlength="11" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="inputPassword4">First Name</label>
+                        <input type="text" class="form-control" id="fname" placeholder="First Name" minlength="5" maxlength="20" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="inputPassword4">Last Name</label>
+                        <input type="text" class="form-control" id="lname" placeholder="Last Name" minlength="5" maxlength="20" required>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                </div>
-                <div class="form-group">
-                    <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" minlength="5" maxlength="100" required>
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
-                    <label for="inputCity">City</label>
-                    <input type="text" class="form-control" id="inputCity">
+                    <label for="inputCity">Destination</label>
+                    <input type="text" class="form-control" id="inputCity" minlength="5" maxlength="20" required>
                     </div>
                     <div class="form-group col-md-4">
-                    <label for="inputState">State</label>
-                    <select id="inputState" class="form-control">
-                        <option selected>Choose...</option>
+                    <label for="inputState">Packages</label>
+                    <select id="inputState" class="form-control" required>
+                        <option selected>Choose Package</option>
                         <option>...</option>
                     </select>
                     </div>
-                    <div class="form-group col-md-2">
-                    <label for="inputZip">Zip</label>
-                    <input type="text" class="form-control" id="inputZip">
-                    </div>
                 </div>
-                <div class="form-group">
-                    <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="gridCheck">
-                    <label class="form-check-label" for="gridCheck">
-                        Check me out
-                    </label>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
+                <br><br>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <br><br>
             </form>
         <?php
         }
